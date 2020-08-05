ff152f6db4dd8178f89199dafcb4f384ff77e8ab
s-weigand <s.weigand.phy@gmail.com>
2020-08-05 15:54:30 +0200

    Added sphinx-copybutton so users can quickly copy example code
    from the docs

--- docs/conf.py
+++ docs/conf.py
@@ -43,7 +43,8 @@ import pytest_localftpserver
 extensions = ['sphinx.ext.autodoc',
               'sphinx.ext.autosummary',
               'sphinx.ext.viewcode',
-              'sphinx.ext.napoleon']
+              'sphinx.ext.napoleon',
+              'sphinx_copybutton']
 
 autoclass_content = "both"
 autosummary_generate = True
@@ -122,6 +123,10 @@ pygments_style = 'sphinx'
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 html_theme = 'sphinx_rtd_theme'
+
+# CopyButton configuration
+copybutton_prompt_text = r">>> |\.\.\. |\$ "
+copybutton_prompt_is_regexp = True
 
 # Theme options are theme-specific and customize the look and feel of a
 # theme further.  For a list of options available for each theme, see the
