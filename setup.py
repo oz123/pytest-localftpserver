149cfca196a8734482972af2dcf38eb550b83915
Oz N Tiram <oz.tiram@gmail.com>
2021-01-27 08:52:28 +0100

    Drop Python3.5 from supported versions

--- setup.py
+++ setup.py
@@ -45,9 +45,10 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
         "Framework :: Pytest",
         'Topic :: Software Development :: Testing',
         "Topic :: Software Development :: Testing :: Mocking"
